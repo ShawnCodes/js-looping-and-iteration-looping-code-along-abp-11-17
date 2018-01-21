85eebe8f23edfea5d4cfe82f7e6eba3bfb4a964e
Shawn T <shawn@huthunt.com>
2018-01-21 20:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = {...driver}
+  delete newDriver[key];
+
+  return newDriver;
+}
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
